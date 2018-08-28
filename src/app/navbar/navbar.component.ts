10beed90bda20e1c6133ff810789c82600868948
kinny94 <ad.sam007@gmail.com>
2018-08-28 10:29:23 -0400

    implemented basic login logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
 	selector: 'navbar',
 	templateUrl: './navbar.component.html',
 	styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-	
-	constructor() { }
-	
-	ngOnInit() {
+export class NavbarComponent{
+
+	user: firebase.User;
+
+	constructor( private afAuth: AngularFireAuth) {
+		afAuth.authState.subscribe( user => this.user = user );
 	}
-	
+
+	logout(){
+		this.afAuth.auth.signOut();
+	}
+
 }
