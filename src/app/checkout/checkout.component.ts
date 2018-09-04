7fd4d809d7e725aaeb229381e11845996086972b
kinny94 <ad.sam007@gmail.com>
2018-09-04 10:54:46 -0400

    order placed form

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+	selector: 'app-checkout',
+	templateUrl: './checkout.component.html',
+	styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+	shipping:any =  {};
+	firstname;
+	lastname;
+	address;
+	city;
+	state;
+	zip;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	save( data ){
+		if( data ){
+			alert("ORder Placed!");
+		}
+	}
+
+	ngOnInit() {
+		this.shipping = new FormGroup({
+			firstname: new FormControl("", Validators.required ),
+			lastname: new FormControl("", Validators.required ),
+			address: new FormControl("", Validators.required ),
+			city: new FormControl("", Validators.required ),
+			state: new FormControl("", Validators.required ),
+			zip: new FormControl("", Validators.required )
+		});
+	}
 
 }
